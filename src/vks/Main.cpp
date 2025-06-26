300210291fef36691a5bf3b3ca9e8b6ce63f0c24
Adrien GIVRY <contact@adrien-givry.com>
2025-06-26 18:11:47 -0400

    Added (hardcoded) image view

--- src/vks/Main.cpp
+++ src/vks/Main.cpp
@@ -106,9 +106,48 @@ int main()
 		const auto& swapChainImages = swapChain->GetImages();
 		const auto& swapChainDesc = swapChain->GetDesc();
 
+		std::vector<VkImageView> swapChainImageViews(swapChainImages.size());
+		for (size_t i = 0; i < swapChainImages.size(); i++)
+		{
+			VkImageViewCreateInfo createInfo{
+				.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+				.image = swapChainImages[i],
+				.viewType = VK_IMAGE_VIEW_TYPE_2D,
+				.format = swapChainDesc.surfaceFormat.format,
+				.components = {
+					.r = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.g = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.b = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.a = VK_COMPONENT_SWIZZLE_IDENTITY
+				},
+				.subresourceRange = {
+					.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+					.baseMipLevel = 0,
+					.levelCount = 1,
+					.baseArrayLayer = 0,
+					.layerCount = 1
+				}
+			};
+
+			if (vkCreateImageView(
+				device.GetLogicalDevice(),
+				&createInfo,
+				nullptr,
+				&swapChainImageViews[i]) != VK_SUCCESS
+			) {
+				throw std::runtime_error("failed to create image views!");
+			}
+		}
+
 		// Draw things here
 
+		for (auto imageView : swapChainImageViews)
+		{
+			vkDestroyImageView(device.GetLogicalDevice(), imageView, nullptr);
+		}
+
 		glfwPollEvents();
+
 	}
 
 	swapChain.reset();
